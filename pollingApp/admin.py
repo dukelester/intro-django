ba8971dc85fa64c562fc51bc7e9dad313ed983a4
dukelester <dukelester4@gmail.com>
2021-06-19 11:58:19 +0300

    Added so many changes to the web app .This include , using generic views, customizing admin and templates etc.

--- pollingApp/admin.py
+++ pollingApp/admin.py
@@ -1,8 +1,36 @@
+import datetime
+
 from django.contrib import admin
+from django.utils import timezone
 
 from . import models
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+# admin.site.register(Question)
+# customizing the admin section
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+    # @admin.display(
+    #     boolean=True,
+    #     ordering='pub_date',
+    #     description='Published recently'
+    # )
+    # def was_published_recent(self):
+    #     now = timezone.now()
+    #     return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    fields = ['choice_text', 'question', 'votes']
+
+
+# admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
--- pollingApp/tests.py
+++ pollingApp/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
+from .models import Question
+
 
 # Create your tests here.
+
+class QuestionModelTest(TestCase):
+    def test_was_published_recently(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.published_recently(), False)
+
+    def test_was_published_old_question(self):
+        time = timezone.now() + datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.published_recently(), False)
+
+    def was_published_new_question(self):
+        time = timezone.now() + datetime.timedelta(hours=23,minutes=59, seconds=50)
+        new_question = Question(pub_date=time)
+        self.assertIs(new_question.published_recently(), True)
+
